ea568bda9c9a41c0732d261f1d434247f2e01794
justin230902 <justin230902@gmail.com>
2024-10-10 17:00:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cursocsharp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");  Console.ReadLine();
+        }
+    }
+}
